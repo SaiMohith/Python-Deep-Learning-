ef12594bf4b0ec252745dd90135e5e0089c6a70b
SaiMohith <mohith299@gmail.com>
2018-01-30 16:49:30 -0600

    sentence commit

--- LabAssignment1/Sentence.py
+++ LabAssignment1/Sentence.py
@@ -1,9 +1,32 @@
-def sentence(inp):
-    words = inp.split()
-    b = len(words)
-    c = int((0+b)/2)
-    print(words[c])
+def mid_word(inp):  # define a function mid_word to find the middle word in the sentence
+    words = inp.split()  # split the sentence into words and store in the list words
+    num = int((len(words)))  # get the length of the list
+    if len(words) % 2 == 0:  # if length is even number
+        print("Middle words: ", words[(int((num / 2)-1)):(int((num / 2)+1))])  # print the middle words
+    else:  # if the length is odd
+        print("Middle Word: ", words[int(num/2)])  # print the median
 
 
-sent = input("enter:")
-sentence(sent)
+def longest_word(inp):  # define a function longest_word to print the longest word in the sentence
+    words = inp.split()  # split the sentence into words in a list
+    n = len(words)  # get the length of the list
+    lengths = []  # create a lengths list
+    for word in words:
+        lengths.append(len(word))  # for every word in the words list append the list of each word in the lengths list
+    i = lengths.index(max(lengths))  # get the index which has the max length in the length list
+    print("Longest word: ", words[i])  # return the longest word
+
+
+def reversed_words(inp):  # define a function to print the sentence with the reversed words
+    words = inp.split() # split the words in the sentence
+    r_words = []  # create a empty list to store the reversed words
+    for word in words:
+        r_words.append(word[::-1])  # for each word in words get the reversed word and append it in the r_words list
+    print("Sentence with reversed words: ", ' '.join(r_words))  # print the joined version of the r_words list
+
+
+# ask the user to enter the sentence
+sent = input("Please enter the sentence to get the middle word, longest word and sentence with reversed words:")
+mid_word(sent)  # call the mid_word function for the user input
+longest_word(sent)  # call the longest_word function
+reversed_words(sent)  # call the reversed_words function
--- LabAssignment1/Verify Password.py
+++ LabAssignment1/Verify Password.py
@@ -1,21 +1,21 @@
-import re
+import re  # import re library
 
 
-def valid_password(pwd):
+def valid_password(pwd):  # define a function valid_password
 
-        if len(pwd) < 6 or len(pwd) > 16:
+        if len(pwd) < 6 or len(pwd) > 16:  # prints if the input has length less than 6 or high than 16
             print("Your Password length should be between 6-16 characters")
-        elif re.search('[0-9]', pwd) is None:
+        elif re.search('[0-9]', pwd) is None:  # prints if pwd doesn't have any of digit between 0-9
             print("Make Sure your Password contains at least one digit")
-        elif re.search('[$@!*]', pwd) is None:
+        elif re.search('[$@!*]', pwd) is None:  # prints if pwd doesn't have any of special characters
             print("Make sure your Password contains at least one of the special characters ($@!*)")
-        elif re.search('[A-Z]', pwd) is None:
+        elif re.search('[A-Z]', pwd) is None:  # prints if pwd doesn't have any of letters between A-Z
             print("Make sure your Password has at least one uppercase letter")
-        elif re.search("[a-z]", pwd) is None:
+        elif re.search("[a-z]", pwd) is None:  # prints if pwd doesn't have any of letters between a-z
             print("Make sure your Password has at least one lowercase letter")
         else:
-            print("Strong Password")
+            print("Strong Password")  # prints if pwd is find
 
 
-p = input("Please Enter your Password: ")
-valid_password(p)
+p = input("Please Enter your Password: ")  # asks the user to enter the password
+valid_password(p)  # calls the function valid_password with user input
